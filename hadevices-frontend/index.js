e5a972ab6ce4ac471b22e1421d445f012d5e2a83
jacquibushfi <info@stainedglassofmckinney.com>
2020-02-12 11:44:28 -0600

    getting the add device button to work

--- hadevices-frontend/index.js
+++ hadevices-frontend/index.js
@@ -1,13 +1,54 @@
-const BASE_URL = 'http://localhost:3000';
-const LOCATIONS_URL = `${BASE_URL}/locations`
-const DEVICES_URL = `${BASE_URL}/rpdevices`
+const BASE_URL = 'http://localhost:3000'
+
+
 
 window.addEventListener('load', () => {
   getLocations()
 })
 
+function displayCreateForm() {
+  let formdiv = document.querySelector('#device-form')
+  let html = `
+   <form onsubmit="createDev()"; return false;>
+   <label>Location Id:</label>
+   <input type="text" id="location_id" name="location_id"></br>
+   <label>Hostname:</label>
+   <input type="text" id="hostname" name="hostname"></br>
+   <label>Ip Address:</label>
+   <input type="text" id="ipadd" name="ipadd"></br>
+   
+   <input type="submit">
+   </form>
+ `
+  formdiv.innerHTML = html
+}
+
+
+function createDev() {
+  const dev = {
+    hostname: document.getElementById('hostname').value,
+    ipadd: document.getElementById('ipadd').value,
+    location_id: document.getElementById('location_id').value
+  }
+  fetch(BASE_URL + `/locations/${dev.location_id}/rpdevices`, {
+    method: "POST",
+    body: JSON.stringify(dev),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+    .then(resp => resp.json())
+    .then(data => {
+      let newDev = new RpDevice(data)
+      const locationContainer = document.querySelector('#location-container');
+      const locationCard = document.querySelector(`ul id=location-card-ul-${dev.location_id}`)
+      newDev.renderDevices(locationCard, locationContainer)
+    })
+}
+
 function removeDevice() {
-  fetch(`${DEVICES_URL}/${event.target.dataset.rpdeviceId}`, {
+  fetch(BASE_URL + `/locations/${event.target.locationCard.dataset.id}/rpdevices/${event.target.dataset.rpdeviceId}`, {
     method: "DELETE",
     headers: {
       'Content-type': 'application/json',
@@ -17,59 +58,59 @@ function removeDevice() {
     .then(event.target.parentElement.remove())
 }
 
-function displayCreateForm(id) {
-  console.log(id)
-  let devFormDiv = document.getElementById("Device-form")
-  let html = `
-    <form onsubmit="createDev(e); return false;">
-    
-    <h3>Location id: ${id}</h3>
+// function displayCreateForm2(id) {
 
-    <input type="text" name="location_id" value=${id}></br>
-    <label>Hostname:</label>
-    <input type="text" id="hostname"></br>
-    <label>Ip Address:</label>
-    <input type="text" id="ipadd"></br>
-    <input type="submit" value="Create Device">
-    </form>
-  `
-  devFormDiv.innerHTML = html
-}
+//   let devFormDiv = document.getElementById("device-form")
+//   let html = `
+//     <form onsubmit="createDev()";return false;>
+//     <label>Location Id:</label>
+//     <input type="text" name="location_id" value=${id}></br>
+//     <label>Hostname:</label>
+//     <input type="text" id="hostname"></br>
+//     <label>Ip Address:</label>
+//     <input type="text" id="ipadd"></br>
+//     <input type="submit" value="Create Device">
+//     </form>
+//   `
+//   devFormDiv.innerHTML = html
+// }
 
-function createDev(e) {
-  e.preventDefault()
-  const dev = {
-    hostname: document.getElementById('hostname').value,
-    ipadd: document.getElementById('ipadd').value,
-    location_id: document.getElementById('location_id').value,
-  }
-  console.log(dev)
-  fetch(BASE_URL + `/locations/${location_id}/rpdevices`), {
-    method: "POST",
-    body: JSON.stringify(dev),
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
-  }
-    .then(resp => resp.json())
-    .then(dev => {
-      let newDev = new RpDevice(dev)
-      const locationContainer = document.querySelector('#location-container');
-      const locationCard = document.querySelector(`ul id=location-card-ul-${location_id}`)
-      newDev.renderDevices(locationCard, locationContainer)
-    })
-}
+
+
+// function createDev2() {
+//   e.preventDefault()
+//   const dev = {
+//     hostname: document.getElementById('hostname').value,
+//     ipadd: document.getElementById('ipadd').value,
+//     location_id: document.getElementById('location_id').value
+//   }
+
+//   fetch(BASE_URL + `/locations/${location_id}/rpdevices`), {
+//     method: "POST",
+//     body: JSON.stringify(dev),
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json'
+//     }
+//   }
+//     .then(resp => resp.json())
+//     .then(data => {
+//       let newDev = new RpDevice(data)
+//       const locationContainer = document.querySelector('#location-container');
+//       const locationCard = document.querySelector(`ul id=location-card-ul-${location_id}`)
+//       newDev.renderDevices(locationCard, locationContainer)
+//     })
+// }
 
 
 function getLocations() {
   fetch(BASE_URL + '/locations')
     .then(resp => resp.json())
     .then(data => {
-      const locationContainer = document.querySelector('#location-container');
+      let locationContainer = document.querySelector('#location-container');
       locationContainer.innerHTML = '';
       data.forEach(location => {
-        console.log(location);
+
         let newLocation = new Location(location);
         newLocation.renderLocation();
       });
@@ -94,7 +135,7 @@ class Location {
 
   renderLocation() {
     const locationContainer = document.querySelector('#location-container');
-    console.log(location);
+
     const locationCard = document.createElement('div');
     locationCard.dataset.id = this.id;
     locationCard.className = 'card';
@@ -109,7 +150,7 @@ class Location {
 
   renderDevices(locationCard, locationContainer) {
     let rpdevices = this.rpdevices
-    console.log(rpdevices);
+
     rpdevices.forEach(rpdevice => {
       const locationCardUl = locationCard.querySelector('ul');
       const rpdeviceLi = document.createElement('li');
