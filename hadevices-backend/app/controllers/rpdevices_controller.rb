142671807f993dad1304f322cc9372ddf63ceb68
jacquibushfi <info@stainedglassofmckinney.com>
2020-02-14 10:52:31 -0600

    cleaning up

--- hadevices-backend/app/controllers/rpdevices_controller.rb
+++ hadevices-backend/app/controllers/rpdevices_controller.rb
@@ -1,36 +1,21 @@
 class RpdevicesController < ApplicationController
-  before_action :set_rpdevice, only: [:show, :update, :destroy]
-
+ 
   # GET /rpdevices
   def index
-      #binding.pry
     rpdevices = Rpdevice.all
-
     render json: rpdevices
   end
 
   # GET /rpdevices/1
   def show
-      #binding.pry
+    rpdevice = Rpdevice.find(params[:id])
     render json: rpdevice
   end
 
   # POST /rpdevices
   def create
-      #binding.pry
     rpdevice = Rpdevice.new(rpdevice_params)
-
     if rpdevice.save
-      render json: rpdevice
-    else
-      render json: rpdevice.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /rpdevices/1
-  def update
-      #binding.pry
-    if rpdevice.update(rpdevice_params)
       render json: rpdevice
     else
       render json: rpdevice.errors, status: :unprocessable_entity
@@ -42,14 +27,25 @@ class RpdevicesController < ApplicationController
     @rpdevice.destroy
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_rpdevice
-      @rpdevice = Rpdevice.find(params[:id])
-    end
+  # # PATCH/PUT /rpdevices/1
+  # def update
+  #   if rpdevice.update(rpdevice_params)
+  #     render json: rpdevice
+  #   else
+  #     render json: rpdevice.errors, status: :unprocessable_entity
+  #   end
+  # end
 
+ # before_action :set_rpdevice
+
+  private
     # Only allow a trusted parameter "white list" through.
     def rpdevice_params
       params.require(:rpdevice).permit(:hostname, :ipadd, :location_id, :id)
     end
+
+     # Use callbacks to share common setup or constraints between actions.
+    # def set_rpdevice
+    #   @rpdevice = Rpdevice.find(params[:id])
+    # end
 end
